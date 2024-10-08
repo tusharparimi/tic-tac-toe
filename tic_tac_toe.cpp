92dd5cade1ed346a1163324cf08d75459662b1c9
[Tushar Parimi] <tushar.parimi@gmail.com>
2024-10-08 20:36:04 +0530

    Fixes issue #2

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -30,6 +30,34 @@ void display_board(char board[ROWS][COLUMNS])
     }
 }
 
+int check_winner(char board[ROWS][COLUMNS], char p1_mark, char p2_mark)
+{                            
+    if ((board[0][0] == p1_mark && board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+        (board[0][2] == p1_mark && board[0][2] == board[1][1] && board[1][1] == board[2][0]))
+    {
+        return 1;
+    }
+    if (board[0][0] == p2_mark && board[0][0] == board[1][1] && board[1][1] == board[2][2] ||
+        (board[0][2] == p2_mark && board[0][2] == board[1][1] && board[1][1] == board[2][0]))
+    {
+        return 2;
+    }
+    for (int i=0; i<ROWS; i++)
+    {
+        if ((board[i][0] == p1_mark && board[i][0] == board[i][1] && board[i][1] == board[i][2]) ||
+            (board[0][i] == p1_mark && board[0][i] == board[1][i] && board[1][i] == board[2][i]))
+        {
+            return 1;
+        }
+        if ((board[i][0] == p2_mark && board[i][0] == board[i][1] && board[i][1] == board[i][2]) ||
+            (board[0][i] == p2_mark && board[0][i] == board[1][i] && board[1][i] == board[2][i]))
+        {
+            return 2;
+        }
+    }
+    return 0;
+}
+
 int main()
 {
     char p1_mark, p2_mark, mark;
@@ -44,17 +72,23 @@ int main()
     origin.X = 0;
     origin.Y = 0;
     COORD curr;
+    int status;
 
     char board[ROWS][COLUMNS] = {{'_', '_', '_'},
                                  {'_', '_', '_'},
                                  {'_', '_', '_'}};
 
-    std::cout << "Starting game..." << std::endl;
+    std::cout << "Game Started..." << std::endl;
     std::cout << "Board status: " << std::endl;
 
     while(g < 9)
     {
+        status = check_winner(board, p1_mark, p2_mark);
         display_board(board);
+        if (status != 0)
+        {
+            break;
+        }
 
         if (turn)
         {
@@ -64,6 +98,7 @@ int main()
         {
             mark = p1_mark;
         }
+        
         std::cout << "Player " << static_cast<int>(turn) + 1 << "'s (" << mark << ") turn..." << std::endl;
         std::cout << "Type desired position: ";
         std::cin >> posx >> posy;
@@ -77,13 +112,32 @@ int main()
         g++;
 
     }
-    
-    display_board(board);
 
-    curr = get_cursor_position();
-    curr.Y = curr.Y + 2;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), curr);
-    std::cout << "Game Finished !!!";
+    if (status != 0)
+    {
+        curr = get_cursor_position();
+        curr.Y = curr.Y - 6;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), curr);
+        display_board(board);
+    }
+    else
+    {
+        curr = get_cursor_position();
+        curr.Y = curr.Y + 2;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), curr);    
+    }
+    if (status == 1)
+        {
+            std::cout << "Game Finished: " << p1_mark << " WINS !!!";
+        }
+    else if (status == 2)
+    {
+        std::cout << "Game Finished: " << p2_mark << " WINS !!!";
+    }
+    else
+    {
+        std::cout << "DRAW !!!";
+    }
 
     return 0;
 }
